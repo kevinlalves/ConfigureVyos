62ccfc80fcf6fc0058fb869d53c7376feedbe19a
Kevin de Lima Alves <kevin.lima@lds.ifce.edu.br>
2021-12-09 17:07:58 -0300

    working version

---
+++ README.md
@@ -0,0 +1,12 @@
+# About
+Implemets a class used for interactive configuration of VyOS router
+
+# Usage
+As of now, you can configure any VyOS system in a host-only network from the host engine. From sut folder, run:
+
+´´´sh
+python3 configure.py <host> <user>
+´´´
+
+# Configuration
+You have to set the appropriate ssh private key in sut
--- sut/configure.py
+++ sut/configure.py
@@ -1,26 +1,52 @@
 import netmiko
+import sys
 
-vyos_router = {
-    'device_type': 'vyos',
-    'host': '192.168.56.101',
-    'username': 'kelaves',
-    'key_file': 'id_rsa',
-    'port': 22,
-}
+class ConfigureVyos(object):
 
+    def __init__(self, host, user, port=22):
+        self.vyos_router = {
+        'device_type': 'vyos',
+        'host': host,
+        'username': user,
+        'key_file': 'id_rsa',
+        'port': port,
+        }
+        self.net_connect = netmiko.ConnectHandler(**self.vyos_router)
+        self.command = []
 
+    def start(self):
+        option = input("configure, run command or quit? (conf/run/q): ")
+        while option != 'q':
+            if option == 'conf':
+                self.command = []
+                cmd_line = input('enter configuration(q/quit): ')
+                while cmd_line not in ('q','quit'):
+                    self.command += [cmd_line]
+                    cmd_line = input('enter configuration(q/quit): ')
+                self.config_commands()
+            elif option == 'run':
+                self.command = input('enter command(q/quit): ')
+                while self.command not in ('q','quit'):
+                    self.run_commands()
+                    self.command = input('enter command(q/quit): ')
+            else :
+                print('Options are only "conf" and "run"')
 
-net_connect = netmiko.ConnectHandler(**vyos_router)
+            option = input("\nconfigure, run command or quit? (conf/run/q): ")
 
-config_commands = [
-    'set interfaces ethernet eth0 description eris',
-]
+    def config_commands(self):
+        output = self.net_connect.send_config_set(self.command, exit_config_mode=False)
+        self.net_connect.commit()
+        print(output)
+        return output
+    
+    def run_commands(self):
+        output = self.net_connect.send_command(self.command)
+        print(output)
+        return output
 
-output = net_connect.send_config_set(config_commands, exit_config_mode=False)
-print(output)
-
-output = net_connect.commit()
-print(output)
-
-output = net_connect.send_command('run show interfaces')
-print(output)
+if __name__ == '__main__':
+    host = sys.argv[1]
+    user = sys.argv[2]
+    vyos_2 = ConfigureVyos(host, user)
+    vyos_2.start()
